00d813875ac30942bff64beb8409a1fcc2ff3c80
Nik885179 <161007818+Nik885179@users.noreply.github.com>
2025-03-19 14:15:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ USER_REPLY_TEXT = os.environ.get("USER_REPLY_TEXT", "Don't send me messages dire
 
 
 #your bot token here from https://telegram.me/BotFather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8101575666:AAENSulkB2z3uHzwLdsJfBghupH3AJ0qj0A") 
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "8101575666:AAGjIr1Cqe7G8MjfiLVvfkNmoc9DVeFTbFs") 
 #your api id from https://my.telegram.org/apps
 APP_ID = int(os.environ.get("APP_ID", "21484586"))
 #your api hash from https://my.telegram.org/apps
